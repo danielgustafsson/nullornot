ef057fb3667f08d1a0d1d3c11dd5f76969c5e26f
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-02-02 15:50:44 +0100

    Add the new strat and use O3 as well

--- test.sh
+++ test.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-printf "\n-O2 optimized\n\n"
-cc -Wall -O2 -DELVIS -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-cc -Wall -O2 -DDEFINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-cc -Wall -O2 -DFUNCTION -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-cc -Wall -O2 -DFUNCTION -DINLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-cc -Wall -O2 -DIFTHENELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-
 printf "\nNo optimization\n\n"
 cc -Wall -DELVIS -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
 cc -Wall -DDEFINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
 cc -Wall -DFUNCTION -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
-cc -Wall -DFUNCTION -DINLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -DFUNCTION -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -DFUNCTION -DFUNCTION_ELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -DFUNCTION -DFUNCTION_ELSE -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
 cc -Wall -DIFTHENELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+printf "\n-O2 optimized\n\n"
+cc -Wall -O2 -DELVIS -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DDEFINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DFUNCTION -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DFUNCTION -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DFUNCTION -DFUNCTION_ELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DFUNCTION -DFUNCTION_ELSE -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O2 -DIFTHENELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+printf "\n-O3 optimized\n\n"
+cc -Wall -O3 -DELVIS -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DDEFINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DFUNCTION -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DFUNCTION -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DFUNCTION -DFUNCTION_ELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DFUNCTION -DFUNCTION_ELSE -DFUNCTION_INLINE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+cc -Wall -O3 -DIFTHENELSE -c nullornot.c -o nullornot.o && gcc -o nullornot nullornot.o && ./nullornot > /dev/null
+
